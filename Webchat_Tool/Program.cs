4162de378c9d89a4eceab9fdfe22781a67705560
FelipeSilveiraIt <felipe.silveira.it@gmail.com>
2021-11-03 23:25:23 -0300

    dotnet console app base

---
+++ Webchat_Tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Webchat_Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
